7ee74cea7fec1595f88751a37b5a027f1a719e7c
Valentin G Luchian <gabriel.luchian89@gmail.com>
2025-02-12 15:50:49 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,75 +1,28 @@
-tterraform {
+# terraform/main.tf
+terraform {
+  required_version = ">= 1.0.0"
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
   }
 }
 
 provider "github" {
-  token = var.github_token
+  # Token stored as environment variable GITHUB_TOKEN
 }
 
+# Example resource (customize as needed)
 resource "github_repository" "blog" {
-  name        = "tech-blog"
-  description = "My Tech Blog"
-  
-  visibility = "public"
-  has_issues = true
-  has_wiki   = true
-  
+  name        = "blog"
+  description = "My tech blog"
+  visibility  = "public"
+
   pages {
     source {
-      branch = "gh-pages"
+      branch = "main"
       path   = "/"
     }
   }
-}
-
-resource "github_branch" "gh_pages" {
-  repository = github_repository.blog.name
-  branch     = "gh-pages"
-}
-
-resource "github_branch_default" "default" {
-  repository = github_repository.blog.name
-  branch     = "main"
-}erraform {
-  required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "~> 4.0"
-    }
-  }
-}
-
-provider "github" {
-  token = var.github_token
-}
-
-resource "github_repository" "blog" {
-  name        = "tech-blog"
-  description = "My Tech Blog"
-  
-  visibility = "public"
-  has_issues = true
-  has_wiki   = true
-  
-  pages {
-    source {
-      branch = "gh-pages"
-      path   = "/"
-    }
-  }
-}
-
-resource "github_branch" "gh_pages" {
-  repository = github_repository.blog.name
-  branch     = "gh-pages"
-}
-
-resource "github_branch_default" "default" {
-  repository = github_repository.blog.name
-  branch     = "main"
 }
