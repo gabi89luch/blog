0fbf0a37a23bb8ab25adb5344e9a2e175051d2f3
Valentin G Luchian <gabriel.luchian89@gmail.com>
2025-02-12 16:13:19 +0000

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -3,3 +3,8 @@ output "repository_url" {
   value       = github_repository.blog.html_url
   description = "URL of the GitHub repository"
 }
+
+output "pages_url" {
+  value       = github_repository.blog.pages[0].html_url
+  description = "GitHub Pages URL"
+}
